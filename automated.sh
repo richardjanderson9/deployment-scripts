5662d15a7c54a488d701fe648efe920eae041746
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2024-05-04 19:16:13 +0100

    Last min check!

--- automated.sh
+++ automated.sh
@@ -6,21 +6,18 @@ if [ "$EUID" -eq 0 ]; then
     exit 1
 fi
 
+## Check if script is run as root!
+if [ "$EUID" -eq 0 ]; then
+    echo "Warning: This script should not be run as root. Please run it as a regular user."
+    exit 1
+fi
+
 ## Set Variables!
 github_profile="https://github.com/richardjanderson9/" ## Github Profile Link!
 github_repository="deployment-scripts" ## Github Repository Name!
 
 ## Construct Github Repository Link!
 github_link="${github_profile}${github_repository}.git"
-
-## Confirm current path!
-echo "Current path: $(pwd)"
-read -p "Is this the correct path to clone the repository? (y/n): " confirm_path
-
-if [[ $confirm_path != "y" && $confirm_path != "Y" ]]; then
-    echo "Please navigate to the correct path and run the script again."
-    exit 1
-fi
 
 ## Clone the repository!
 echo "Cloning repository..."
@@ -33,13 +30,14 @@ fi
 
 ## Change directory!
 echo "Changing directory to $github_repository..."
-cd "$github_repository" || { echo "Failed to change directory. Please make sure the directory exists."; exit 1; }
-
-## Execute get-started.sh if it exists!
-get_started_script="./get-started.sh"
-if [ -f "$get_started_script" ]; then
-    echo "Executing get-started.sh script..."
-    bash "$get_started_script"
-else
-    echo "get-started.sh script not found in the current directory."
-fi
+cd "$github_repository" && {
+    echo "Successfully changed directory."
+    ## Execute get-started.sh if it exists!
+    get_started_script="./get-started.sh"
+    if [ -f "$get_started_script" ]; then
+        echo "Executing get-started.sh script..."
+        bash "$get_started_script"
+    else
+        echo "get-started.sh script not found in the current directory. Make sure it exists and try again."
+    fi
+} || echo "Failed to change directory. Please make sure the directory exists."
